cac24ff9c7a4818637f2f8452edf4d82519de930
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-01 01:55:47 +0800

    updated the url naming and added other views

--- coffeenijuan/login_system/urls.py
+++ coffeenijuan/login_system/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "login_system"
 urlpatterns = [
     path('', views.home, name='index'),
-    path('home', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
--- coffeenijuan/login_system/views.py
+++ coffeenijuan/login_system/views.py
@@ -9,3 +9,15 @@ def home(request):
         "csss" : css,
         "jss"  : js
     })
+
+def login(request):
+    return render(request, "login_system/login.html", {
+        "csss" : css,
+        "jss"  : js
+    })
+
+def register(request):
+    return render(request, "login_system/register.html", {
+        "csss" : css,
+        "jss"  : js
+    })
