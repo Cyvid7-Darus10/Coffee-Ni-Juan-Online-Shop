3e5c9a679bab89dc2ab41b22afe01201f98359af
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-01 21:55:00 +0800

    Arranged the file structure and added templates

---
+++ coffeenijuan/account/forms.py
@@ -0,0 +1,14 @@
+# from dataclasses import fields
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+
+# from account.models import Account
+
+
+# class RegistrationForm(UserCreationForm):
+#     email = forms.EmailField(max_length=60, help_text='This field is required. Add a valid email address.')
+
+#     class Meta:
+#         model = Account
+#         fields = ("email", "username", "pass1", "pass2")
+
--- coffeenijuan/account/views.py
+++ coffeenijuan/account/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+# from account.forms import RegistrationForm
 
 js = []
 css = []
