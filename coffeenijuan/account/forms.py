561a5801d87c41155f9d4318e39d12d49dfd36a8
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-04 22:37:07 +0800

    added the login function

--- coffeenijuan/account/forms.py
+++ coffeenijuan/account/forms.py
@@ -13,3 +13,18 @@ class RegistrationForm(UserCreationForm):
         fields = ('email', 'username', 'password1', 'password2')
 
 
+class AccountAuthenticationForm(forms.ModelForm):
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ('email', 'password')
+
+	def clean(self):
+		if self.is_valid():
+			email = self.cleaned_data['email']
+			password = self.cleaned_data['password']
+			if not authenticate(email=email, password=password):
+				raise forms.ValidationError("Invalid login")
+
+
--- coffeenijuan/account/views.py
+++ coffeenijuan/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 js = []
 css = []
@@ -12,12 +12,35 @@ def home(request):
     })
 
 def login_view(request):
+    if request.user.is_authenticated: 
+        return redirect("account:home")
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("account:home")
+    else:
+        form = AccountAuthenticationForm()
+
+    login_form = form
+
     return render(request, "account/login.html", {
         "csss" : css,
-        "jss"  : js
+        "jss"  : js,
+        "login_form" : login_form
     })
 
+
 def register(request):
+    if request.user.is_authenticated: 
+        return redirect("account:home")
+
     if request.POST:
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -40,5 +63,7 @@ def register(request):
     })
 
 def logout_view(request):
-	logout(request)
-	return redirect('account:home')
+    if request.user.is_authenticated: 
+        logout(request)
+        
+    return redirect('account:home')
