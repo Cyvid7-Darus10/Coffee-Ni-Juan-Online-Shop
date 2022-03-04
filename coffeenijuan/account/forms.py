d099f249a80e004ad968de19bd3592a4ce27e8f0
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-04 21:44:47 +0800

    Added form for registration

--- coffeenijuan/account/forms.py
+++ coffeenijuan/account/forms.py
@@ -1,14 +1,15 @@
-# from dataclasses import fields
-# from django import forms
-# from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 
-# from account.models import Account
+from account.models import Account
 
 
-# class RegistrationForm(UserCreationForm):
-#     email = forms.EmailField(max_length=60, help_text='This field is required. Add a valid email address.')
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=60, help_text='Valid Email Required')
 
-#     class Meta:
-#         model = Account
-#         fields = ("email", "username", "pass1", "pass2")
+    class Meta:
+        model = Account
+        fields = ('email', 'username', 'pwd1', 'pwd2')
+
 
