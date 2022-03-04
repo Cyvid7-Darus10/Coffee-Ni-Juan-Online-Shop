5b5554b3365b48245e2fdb89ef80d28fd381729a
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-04 21:39:06 +0800

    Added datatpe into the account table

--- coffeenijuan/account/admin.py
+++ coffeenijuan/account/admin.py
@@ -4,8 +4,8 @@ from account.models import Account
 
 
 class AccountAdmin(UserAdmin):
-	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
-	search_fields = ('email','username',)
+	list_display = ('email','username','date_joined','account_type', 'last_login', 'is_admin','is_staff')
+	search_fields = ('email','username')
 	readonly_fields=('date_joined', 'last_login')
 
 	filter_horizontal = ()
--- coffeenijuan/account/models.py
+++ coffeenijuan/account/models.py
@@ -40,6 +40,7 @@ class Account(AbstractBaseUser):
 	is_active				= models.BooleanField(default=True)
 	is_staff				= models.BooleanField(default=False)
 	is_superuser			= models.BooleanField(default=False)
+	account_type			= models.CharField(max_length=60, default="customer")
 
 
 	USERNAME_FIELD = 'email'
