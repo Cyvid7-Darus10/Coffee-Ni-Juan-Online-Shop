e52631bb929a842cbc9ace5a8acde20efcf11cd5
cerebrus6 <dcyap@up.edu.ph>
2022-03-12 21:36:44 +0800

    Added login page frontend and form

--- coffeenijuan/account/forms.py
+++ coffeenijuan/account/forms.py
@@ -6,19 +6,21 @@ from account.models import Account
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(max_length=60, help_text='Valid Email Required')
+    email = forms.EmailField(max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'Email'}), help_text='Valid Email Required')
 
     class Meta:
         model = Account
         fields = ('email', 'username', 'first_name', "last_name", 'password1', 'password2')
 
-
 class AccountAuthenticationForm(forms.ModelForm):
-	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
 	class Meta:
 		model = Account
 		fields = ('email', 'password')
+		widgets = {
+        	'email': forms.EmailInput(attrs={'placeholder': 'Email'})
+    	}
 
 	def clean(self):
 		if self.is_valid():
