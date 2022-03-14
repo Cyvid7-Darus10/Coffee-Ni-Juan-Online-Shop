55b44d88f2e73acaf6628117c6d15d21dd40ec13
jeff <jlayahin2@up.edu.ph>
2022-03-15 02:16:08 +0800

    Registrations frontend updates

--- coffeenijuan/account/forms.py
+++ coffeenijuan/account/forms.py
@@ -7,11 +7,27 @@ from account.models import Account
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'Email'}), help_text='Valid Email Required')
-
+    username = forms.CharField(max_length=60,widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    first_name = forms.CharField(max_length=60,widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=60,widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    password1 = forms.CharField(max_length=60,widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=60,widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+    
     class Meta:
         model = Account
         fields = ('email', 'username', 'first_name', "last_name", 'password1', 'password2')
-
+        
+   
+    def __init__(self, *args, **kwargs): 
+        super(RegistrationForm,self).__init__(*args, **kwargs)
+        self.fields['email'].label = ''
+        self.fields['username'].label = ''
+        self.fields['first_name'].label = ''
+        self.fields['last_name'].label = ''
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
+    
+        
 class AccountAuthenticationForm(forms.ModelForm):
 	password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
