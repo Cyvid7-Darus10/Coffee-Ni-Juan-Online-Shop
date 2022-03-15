d0df15d045709b7736cf15dcda6d87d768a72d9c
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-16 05:43:00 +0800

    forgot password route

--- coffeenijuan/account/urls.py
+++ coffeenijuan/account/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
+    path('forgot_password', views.forgot_password, name='forgot_password'),
     path('verify/<str:token>/', views.verify, name='verify'),
     path('prompt_message/<str:type>/', views.prompt_message, name='prompt_message'),
 ]
