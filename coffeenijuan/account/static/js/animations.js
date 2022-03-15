00ec0902234e01fbe44df16ed92e345a842e0ae9
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-16 03:59:00 +0800

    refactored the js for forms

--- coffeenijuan/account/static/js/animations.js
+++ coffeenijuan/account/static/js/animations.js
@@ -1,4 +1,4 @@
-$( ".animation-btn" ).mouseenter(function(e) {
+$(".animation-btn").mouseenter(function(e) {
     var parentOffset = $(this).offset(); 
 
     var relX = e.pageX - parentOffset.left;
@@ -9,7 +9,7 @@ $( ".animation-btn" ).mouseenter(function(e) {
 
 });
 
-$( ".animation-btn" ).mouseleave(function(e) {
+$(".animation-btn").mouseleave(function(e) {
     var parentOffset = $(this).offset(); 
     var relX = e.pageX - parentOffset.left;
     var relY = e.pageY - parentOffset.top;
@@ -18,26 +18,3 @@ $( ".animation-btn" ).mouseleave(function(e) {
     $(this).prev(".su_button_circle").addClass("desplode-circle");
 });
 
-$("#id_email").blur(function() {
-    if($(this).val()){
-        $("#id_username").addClass("show");
-    } else {
-        $("#id_username").removeClass("show");
-    }
-});
-
-$("#id_username").blur(function() {
-    if($(this).val()){
-        $("#id_first_name").addClass("show");
-    } else {
-        $("#id_first_name").removeClass("show");
-    }
-});
-
-$("#id_first_name").blur(function() {
-    if($(this).val()){
-        $("#id_last_name").addClass("show");
-    } else {
-        $("#id_last_name").removeClass("show");
-    }
-});
