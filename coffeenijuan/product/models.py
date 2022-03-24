46b65a15c3c9eaa9800d2758767590819831cde5
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-25 03:33:21 +0800

    added new model for products

--- coffeenijuan/product/models.py
+++ coffeenijuan/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+class Base(models.Model):
+    created = DateTimeField(default=datetime.now)
+    updated = DateTimeField(default=datetime.now)
+    deleted = DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+class Product(Base):
+    label   = models.CharField(max_length=250)
+    date    = models.DateField()
+
+    def __str__(self):
+        return f"{self.label} on {self.date} with {self.points} points"
