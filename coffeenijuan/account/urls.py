5affc5db4659ca592396269edb167822dfb0da95
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-03-26 03:01:43 +0800

    updated the route for about view

--- coffeenijuan/account/urls.py
+++ coffeenijuan/account/urls.py
@@ -6,6 +6,7 @@ app_name = "account"
 urlpatterns = [
     path('', views.home, name='index'),
     path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
