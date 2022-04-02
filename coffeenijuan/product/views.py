0453f8173ee8c4fe8faf02771e7a7f1864498f69
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-03 02:03:13 +0800

    updated the backend for product_item

--- coffeenijuan/product/views.py
+++ coffeenijuan/product/views.py
@@ -1,9 +1,19 @@
+from math import ceil
 from django.shortcuts import render
-from .forms import productOrder
+from .models import Product
 
 # global variables for js and css
 js = []
 css = []
+
+# helper functions
+def get_if_exists(model, **kwargs):
+    try:
+        obj = model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        obj = None
+    return obj
+
 
 def product_list(request):
     return render(request, "product/product_list.html", {
@@ -11,8 +21,18 @@ def product_list(request):
         "jss"  : js
     })
 
-def product_item(request):
+def product_item(request, id):
+    product = get_if_exists(Product, **{'id':id})
+    
+    rating = product.rating
+    not_whole = rating % 1
+    rating = int(rating)
+
     return render(request, "product/product_item.html", {
-        "csss" : css,
-        "jss"  : js,
+        "csss"     : css,
+        "jss"      : js,
+        "product"  : product,
+        "n"        : range(rating),
+        "n2"       : range(5 - (rating + (1 if not_whole else 0))),
+        'not_whole': not_whole
     })
