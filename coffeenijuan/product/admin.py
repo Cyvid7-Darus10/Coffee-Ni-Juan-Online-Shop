f9eb4ef663e99892a7af9a12bf428f55affc5b50
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-04 13:25:51 +0800

    fix(database): removed the filter for label

--- coffeenijuan/product/admin.py
+++ coffeenijuan/product/admin.py
@@ -4,9 +4,9 @@ from .models import Product, Review, Comment
 # Created the class for defining the admin view for Product model
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('label', 'price', 'stock', 'created', 'updated')
-    list_filter = ('label', 'created', 'updated')
+    list_filter = ('created', 'updated')
     list_editable = ('stock', 'price')
-    search_fields = ('name', 'created')
+    search_fields = ('label', 'created')
 
     class Meta:
         model = Product
