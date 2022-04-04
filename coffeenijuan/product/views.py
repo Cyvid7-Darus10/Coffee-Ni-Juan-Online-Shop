e01f41b62145711dbb30af2eac82d3a5f001722c
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-04 14:08:26 +0800

    feat!(product filter): added a filter for product list

--- coffeenijuan/product/views.py
+++ coffeenijuan/product/views.py
@@ -1,6 +1,7 @@
 from math import ceil
 from django.shortcuts import render
 from .models import Product
+from django.http import HttpResponse
 
 # global variables for js and css
 js = []
@@ -16,8 +17,21 @@ def get_if_exists(model, **kwargs):
 
 
 def product_list(request):
-    # get all products
-    products = Product.objects.all()
+    # check if there is post request
+    if request.method == "POST":
+        # use the filter data to get the product list
+        max = request.POST.get('max', None)
+        max = max if max else 1000.00
+        min = request.POST.get('min', 0)
+        min = min if min else 0.00
+        rating = request.POST.get('rating', 0)
+        rating = rating if rating else 0.00
+
+        products = Product.objects.filter(price__gte=min, price__lte=max, rating__gte=rating)
+    else:
+        # get all products
+        products = Product.objects.all()
+
     return render(request, "product/product_list.html", {
         "csss" : css,
         "jss"  : js,
