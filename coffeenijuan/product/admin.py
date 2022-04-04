b619d18425f1e2b12551c0e111b1ea9de26457fa
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-04 13:20:59 +0800

    feat!(database): added a table lookup/filter for products

--- coffeenijuan/product/admin.py
+++ coffeenijuan/product/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Product, Review, Comment
 
-admin.site.register(Product)
+# Created the class for defining the admin view for Product model
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('label', 'price', 'stock', 'created', 'updated')
+    list_filter = ('label', 'created', 'updated')
+    list_editable = ('stock', 'price')
+    search_fields = ('name', 'created')
+
+    class Meta:
+        model = Product
+
+# Registering the models.
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
 admin.site.register(Comment)
--- coffeenijuan/product/models.py
+++ coffeenijuan/product/models.py
@@ -14,16 +14,16 @@ class Base(models.Model):
 class Product(Base):
     label       = models.CharField(max_length=250, blank=True, null=True)
     image_url   = models.CharField(max_length=150, null=True)
-    date        = models.DateField(blank=True, null=True)
+    price       = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    stock       = models.PositiveIntegerField(default=0)
     rating      = models.FloatField(blank=True, null=True)
-    price       = models.FloatField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.label} on {self.date} with {self.price} price"
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['-created']
 
 
 class Review(Base):
