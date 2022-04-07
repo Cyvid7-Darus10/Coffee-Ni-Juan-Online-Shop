e3efc16af6ceb7c0b93db5ef24ee740e6911bc01
cerebrus6 <dcyap@up.edu.ph>
2022-04-07 23:03:08 +0800

    build(table): Order Table, payment models, admin, views.

--- coffeenijuan/payment/models.py
+++ coffeenijuan/payment/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
+from django.db.models.fields import DateTimeField
+from django.contrib.postgres.fields import ArrayField
+from product.models import Product
 
-# Create your models here.
+class Base(models.Model):
+    created = DateTimeField(default=datetime.now)
+    updated = DateTimeField(default=datetime.now)
+    deleted = DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+class Order(Base):
+    label = models.CharField(max_length=250, blank=True, null=True)
+    order_id = models.CharField(max_length=250, blank=True, null=True)
+    customer_id = models.CharField(max_length=30, null=True)
+    address = models.CharField(max_length=30, default="None")
+    status = models.CharField(max_length=30, null=True)
+    total_payment = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.label} initiated on {self.created} with status {self.status} and total payment of {self.total_payment}"
+    
+    class Meta:
+        ordering = ['-created']
--- coffeenijuan/payment/views.py
+++ coffeenijuan/payment/views.py
@@ -1,11 +1,20 @@
 from math import ceil
 from django.shortcuts import render
 from product.models import Product
+from .models import Order
 from django.http import HttpResponse
 
 # global variables for js and css
 js = []
 css = []
+
+# helper function
+def get_if_exists(model, **kwargs):
+    try:
+        obj = model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        obj = None
+    return obj
 
 def shopping_cart(request):
 
@@ -15,8 +24,9 @@ def shopping_cart(request):
     })
 
 def check_out(request):
-
+    order = get_if_exists(Product, **{'id':id})
     return render(request, "payment/check_out.html", {
         "csss" : css,
-        "jss"  : js
+        "jss"  : js,
+        "order" : order
     })
