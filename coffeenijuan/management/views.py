29ed6706c26287968bdf0d66ef1b98f6c4030d9e
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-14 01:21:30 +0800

    feat(inventory): created the backend for inventory

--- coffeenijuan/management/views.py
+++ coffeenijuan/management/views.py
@@ -1,6 +1,11 @@
+from cProfile import label
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate
-from account.forms import RegistrationForm, AccountAuthenticationForm
+from sympy import product
+from account.forms import AccountAuthenticationForm
+from product.models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # global variables for js and css
 js = []
@@ -55,10 +60,57 @@ def overview(request):
 
 def inventory(request):
     page = "inventory"
+
+    label = request.GET.get('label')
+
+    sort = request.GET.get('sort')
+    # split the sort by '-'
+    if sort:
+        sort = sort.split('-')
+        sort_order = sort[0]
+        sort_type = sort[1]
+
+    extra_query = ""
+
+    # get all products from the database
+    if label:
+        product_list = Product.objects.filter(label__icontains=label)
+        extra_query = "&label=" + label
+    
+    if sort:
+        if label:
+            product_object = product_list
+        else:
+            product_object = Product.objects.all()
+
+        if sort_order == "asc":
+            product_list = product_object.order_by(sort_type)
+            if not label:
+                extra_query = "&sort=asc-" + sort_type
+        else:
+            product_list = product_object.order_by('-' + sort_type)
+            if not label:
+                extra_query = "&sort=desc-" + sort_type
+
+    if not sort and not label:
+        product_list = Product.objects.all()
+
+    # paginate the products
+    paginator = Paginator(product_list, 5)
+    page_number = request.GET.get('page')
+    try:
+        products = paginator.page(page_number)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+        
     return render(request, "management/inventory.html", {
         "csss" : css,
         "jss"  : js,
-        "page" : page
+        "page" : page,
+        "products" : products,
+        "extra_query" : extra_query
     })
 
 
