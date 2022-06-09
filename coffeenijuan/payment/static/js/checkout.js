6e19b1538c8e5837e0391f6293bc9f64835c5bca
cerebrus6 <dcyap@up.edu.ph>
2022-06-09 11:22:37 +0800

    Added details in admin@gmail.com account to see how it reflects in the site. Added checker for no payment method in checkout and added checker for no product in checkout. Added autofill for address and mobile number in checkout.

--- coffeenijuan/payment/static/js/checkout.js
+++ coffeenijuan/payment/static/js/checkout.js
@@ -5,58 +5,63 @@ $(document).ready(function(){
 
       $(".payment_option_input").prop("value", opval);
 
-      if(opval=="online"){ 
-        $('#onlinePayment').modal("show");
-      }else if(opval=="bank"){ 
-        $('#bankTransfer').modal("show"); 
-      }else{
-        $("#proof_exist").prop("value", "None");
-      }
-        console.log($("#proof").prop("value"));
+      // if(opval=="online"){ 
+      //   $('#onlinePayment').modal("show");
+      // }else if(opval=="bank"){ 
+      //   $('#bankTransfer').modal("show"); 
+      // }else{
+      //   $("#proof_exist").prop("value", "None");
+      // }
+      //   console.log($("#proof").prop("value"));
   });
 
   $(".confirm_submit").on("click", function() {
-      if ($("#proof_exist").prop("value") != "None"){
-        $("#proof_exist").prop("value", "Not None");
-      }else{
-        $("#proof_exist").prop("value", "None");
-      }
+      // if ($("#proof_exist").prop("value") != "None"){
+      //   $("#proof_exist").prop("value", "Not None");
+      // }else{
+      //   $("#proof_exist").prop("value", "None");
+      // }
     $('.submit_payment_form').trigger("click");
   })
 
   $('.place_order').on("click", function() { 
-      var opval = $("#payment_method").val(); 
-      if(opval=="Select"){ 
+      let opval = $("#payment_method").val(); 
+      // console.log(opval)
+      let len = $(".product").length;
+
+      if(len == 0) {
+        $('#noProduct').modal("show"); 
+      } else if(opval=="select"){ 
         $('#noPaymentOption').modal("show"); 
       } else {
         $('#defaultModal').modal("show"); 
       }
   });
 
-  $("#save_file1, #save_file2").on("click", function() {
-    let mainInput = $("#proof");
-    let id;
-    if(this.id =="save_file1")
-      id = "file1";
-    else if(this.id =="save_file2")
-      id = "file2";
+  // $("#save_file1, #save_file2").on("click", function() {
+  //   let mainInput = $("#proof");
+  //   let id;
+  //   if(this.id =="save_file1")
+  //     id = "file1";
+  //   else if(this.id =="save_file2")
+  //     id = "file2";
 
-    let secondaryInput = $("#"+id);
-    let copy1 = mainInput.clone()
-    let copy2 = secondaryInput.clone()
+  //   let secondaryInput = $("#"+id);
+  //   let copy1 = mainInput.clone()
+  //   let copy2 = secondaryInput.clone()
 
-    if(mainInput.val()!=secondaryInput.val()) {
-      mainInput.after(copy2)
-      mainInput.remove()
-      secondaryInput.after(copy1)
-      secondaryInput.remove()
+  //   if(mainInput.val()!=secondaryInput.val()) {
+  //     mainInput.after(copy2)
+  //     mainInput.remove()
+  //     secondaryInput.after(copy1)
+  //     secondaryInput.remove()
 
-      $("#proof").attr("id", "temp");
-      $("#"+id).attr("id", "proof");
-      $("#temp").attr("id", id);
+  //     $("#proof").attr("id", "temp");
+  //     $("#"+id).attr("id", "proof");
+  //     $("#temp").attr("id", id);
 
-      $("#"+id).css("display", "inline");
-      $("#proof").css("display", "none");
-    }
-  })
+  //     $("#"+id).css("display", "inline");
+  //     $("#proof").css("display", "none");
+  //   }
+  // })
 });
