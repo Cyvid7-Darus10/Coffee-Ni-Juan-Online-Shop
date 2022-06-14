6fd65e8d36cf3d821819a6302f54b7e61e3bf7d1
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-06-14 23:58:13 +0800

    Fixed verification bug

--- coffeenijuan/account/support.py
+++ coffeenijuan/account/support.py
@@ -4,8 +4,6 @@ from management.models import Analytic
 
 LETTERS = config('ENCRYPT_LETTERS')
 WORD = config('SECRET_WORD')
-KEY = config('ENCRYPT_KEY')
-key = KEY.split(',')
 
 
 def get_if_exists(model, **kwargs):
@@ -26,66 +24,25 @@ def record_analytic(request, action_type, description):
 
 
 def encrypt(message):
-    encrypted = encrypt_recur(message, 0, 1)
-    # encrypted = second_layer(encrypted)
-    encrypted = second_layer(encrypted)
-    encrypted = encrypt_recur(encrypted, 0, 1)
-    return encrypted
+    encrypted_message = ''
+    for char in message:
+        if char in LETTERS:
+            index = LETTERS.index(char)
+            encrypted_message += WORD[index]
+        else:
+            encrypted_message += char
+    
+    return encrypted_message
+    
     
 
 def decrypt(message):
-    decrypted = decrypt_recur(message, 0, 1)
-    # decrypted = decrypted.replace(WORD, '')
-    decrypted = decrypted.replace(WORD, '')
-    decrypted = decrypt_recur(decrypted, 0, 1)
-    return decrypted
+    decrypted_message = ''
+    for char in message:
+        if char in WORD:
+            index = WORD.index(char)
+            decrypted_message += LETTERS[index]
+        else:
+            decrypted_message += char
 
-
-def encrypt_recur(message, start, over):
-    if start == over: return message
-    else: start += 1
-    
-    encrypted = ''
-    i = 0
-    for chars in message:
-        if chars in LETTERS:
-            if i == len(key) - over:
-                i = 0
-            num = LETTERS.find(chars)
-            num += int(key[i + start])
-            encrypted +=  LETTERS[num]
-            i += 1
-            
-    return encrypt_recur(encrypted, start, over)
-
-
-def second_layer(message):
-    encrypted = ''
-    i = 0
-    for chars in message:
-        if chars in LETTERS:
-            if i == len(key):
-                i = 0
-            if key[i] == '9':
-                encrypted += WORD
-            encrypted += chars
-            i += 1
-    return encrypted
-
-
-def decrypt_recur(message, start, over):
-    if start == over: return message
-    else: start += 1
-        
-    decrypted = ''
-    i = 0
-    for chars in message:
-        if chars in LETTERS:
-            if i == len(key) - over:
-                i = 0
-            num = LETTERS.find(chars)
-            num -= int(key[i + start])
-            decrypted +=  LETTERS[num]
-            i += 1
-            
-    return decrypt_recur(decrypted, start, over)
+    return decrypted_message
--- coffeenijuan/coffeenijuan/settings.py
+++ coffeenijuan/coffeenijuan/settings.py
@@ -30,7 +30,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-ENCRYPT_KEY = config('ENCRYPT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
