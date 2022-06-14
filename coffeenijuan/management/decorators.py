24ba99f2843fbbef57269ed6dfa0997e1a54fd4d
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-06-14 17:22:59 +0800

    css and account bug fixes

--- coffeenijuan/management/decorators.py
+++ coffeenijuan/management/decorators.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 
 def admin_only(view_func):
     def wrapper_function(request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.is_admin:
+        if request.user.is_authenticated and (request.user.is_admin or request.user.account_type == "admin"):
             return view_func(request, *args, **kwargs)
         else:
             messages.add_message(request, messages.ERROR, 'You are not authorized to access the previous page') 
